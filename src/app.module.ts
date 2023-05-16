1ce4a9bf983b4bd7bc94017da73f32a65b842d33
Taras Leshchuk <taras.l.as@agiliumlabs.com>
2023-05-16 22:10:53 +0300

    add accurate country and city

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common'
+import { HttpModule } from '@nestjs/axios'
+import { Logger, Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-    imports: [],
+    imports: [HttpModule],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService, Logger],
 })
 export class AppModule {
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,36 @@
+import { HttpService } from '@nestjs/axios'
 import { Injectable } from '@nestjs/common'
 import { Request } from 'express'
 import { lookup } from 'geoip-lite'
 import * as requestIp from 'request-ip'
 import * as UAParser from 'ua-parser-js'
 
+interface Location {
+    country_name: string
+    city: string
+}
+
 @Injectable()
 export class AppService {
-    getData(req: Request) {
-        const clientIp = requestIp.getClientIp(req)
-        const uaParserResult = new UAParser(req.headers['user-agent']).getResult()
-        const geo = lookup(clientIp)
-        const country = new Intl.DisplayNames(['en'], { type: 'region' }).of(geo?.country ?? '')
-        const city = geo?.city ?? ''
-        const system = uaParserResult.os.name ?? ''
-        const browser = uaParserResult.browser.name ?? ''
-        return { system, browser, country, city, clientIp }
+    
+    constructor(private httpService: HttpService) {
     }
+    
+    async getData(req: Request) {
+        const uaParserResult = new UAParser(req.headers['user-agent']).getResult()
+        const ip = requestIp.getClientIp(req)
+        const geo = lookup(ip)
+        const location = await this.httpService.axiosRef.get<Location>(`https://ipapi.co/${ip}/json`)
+        
+        return {
+            ip,
+            os: uaParserResult.os.name ?? '',
+            browser: uaParserResult.browser.name ?? '',
+            country: new Intl.DisplayNames(['en'], { type: 'region' }).of(geo?.country ?? ''),
+            city: geo?.city ?? '',
+            accurateCountry: location.data.country_name ?? '',
+            accurateCity: location.data.city ?? '',
+        }
+    }
+    
 }
